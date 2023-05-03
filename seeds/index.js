69b332067603beb385c0c4b4574b7976d874b078
SockBowl <29491331+SockBowl@users.noreply.github.com>
2023-05-03 08:30:26 -0600

    move reviews array

--- seeds/index.js
+++ seeds/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const cloudinary = require('cloudinary').v2;
 require('dotenv').config();
 const cities = require('./cities');
-const { places, descriptors } = require('./seedHelpers');
+const { places, descriptors, reviews } = require('./seedHelpers');
 const Campground = require('../models/campground');
 const Review = require('../models/review');
 const User = require('../models/user');
@@ -14,22 +14,6 @@ db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
   console.log('Database connected');
 });
-
-const reviews = [
-  'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.',
-  'Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus.',
-  'Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.',
-  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-  'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-  'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-  'Duis tempus enim non ante facilisis congue.',
-  'Nunc imperdiet ut enim porttitor laoreet.',
-  'Cras tincidunt, ligula vel pellentesque gravida, urna nunc aliquam sem, sed mollis ipsum enim sit amet arcu.',
-  'Ut venenatis posuere libero accumsan malesuada.',
-  'Praesent venenatis cursus velit vel malesuada.',
-  'Nam iaculis mauris in massa sollicitudin sagittis.',
-  'Vivamus ac nunc rhoncus, efficitur sem et, ornare mauris.'
-];
 
 const randomReview = async (users) => {
   return {
